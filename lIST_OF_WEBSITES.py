e0f7a73979639826607720dfbc34bf538704026b
parulverma14 <77003652+parulverma14@users.noreply.github.com>
2021-03-06 20:27:43 +0530

    Update lIST_OF_WEBSITES.py

--- lIST_OF_WEBSITES.py
+++ lIST_OF_WEBSITES.py
@@ -3,6 +3,11 @@ from googlesearch import search
 import os
 
 url="https://www.google.com/search?sxsrf=ALeKk02AwiAJq-2dDPSYoO8s4s8Pj3kI2w%3A1614877297448&ei=cRJBYJX0GrbZz7sP0rSggAI&"
+user_agent={"User-Agent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.72 Safari/537.36',
+            "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Accept-Charset":"ISO-8859-1,utf-8;q=0.7,*;q=0.3",
+            "Accept-Encoding": "none",
+            "Accept-Language":"en-US,en;q=0.8"}
 
 #websites="list_of_websites"
 def folderList ():
